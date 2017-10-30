e06c263a984c421ecfadaebcf2cbbf8cd9ef0534
Whitney Walker <whimsicalwhitney@gmail.com>
2017-10-30 11:30:10 -0700

    Update copy function to reflect changes to schema

--- initialData.sql
+++ initialData.sql
@@ -5,4 +5,4 @@ COPY player_info (first_name, last_name, team_id) FROM '/Users/whitney/hr/thesis
 --game data
 COPY game_info (home_team_id, away_team_id, game_date) FROM '/Users/whitney/hr/thesis/data/game_info.csv' WITH (FORMAT csv);
 --play data
-COPY play_info (game_id, play_type_id, player_id, points, home_score, away_score, play_length) FROM '/Users/whitney/hr/thesis/data/play_info.csv' WITH (FORMAT csv);
+COPY play_info (game_id, play_type_id, player_id, points, home_score, away_score, play_length, total_game_time) FROM '/Users/whitney/hr/thesis/data/play_info.csv' WITH (FORMAT csv);
