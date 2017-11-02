23fdeeebfaeef2f726adc7c3ac9633bb30968678
Whitney Walker <whimsicalwhitney@gmail.com>
2017-11-01 17:56:11 -0700

    Add new endpoint

--- server/server.js
+++ server/server.js
@@ -1,28 +1,43 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var helpers = require('./helpers.js');
+var q = [];
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// // parse application/x-www-form-urlencoded
-// app.use(bodyParser.urlencoded({ extended: true }));
+// parse application/json
+app.use(bodyParser.json());
 
-// // parse application/json
-// app.use(bodyParser.json());
-
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({extended:true, limit:'50mb'}));
+// app.use(bodyParser.json({limit: '50mb'}));
+// app.use(bodyParser.urlencoded({extended:true, limit:'50mb'}));
 
 app.get('/', function(req, res){
   res.send('hello world');
 });
 
-var receiveGame = (req, res, next) => {
-  // console.log('response>>>>', req)
-  next();
-}
+/*RECEIPT PROCCESS
 
-app.post('/newgame', receiveGame, function(req, res){
-  //figure out how to handle receipt of many individual play by plays
-  console.log('response>>>>', req.body)
+receive game info
+push to q
+check to see if game at time with same players has been played/inserted into db 
+if so, get id and insert into db with play info
+if not, create new game and get id
+  save game id, team, start date in memory to reference wo querying db
+save plays
+
+
+
+
+
+*/
+
+
+// app.post('/newgame', helpers.checkGameExists, helpers.addGame, helpers.addPlay, function(req, res){
+//   res.send('game received');
+// });
+app.post('/newgame', function(req, res){
+  q.push(req.body)
   res.send('game received');
 });
 
