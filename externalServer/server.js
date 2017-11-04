94f53f0efed332e1f0df3b65fdb9e36ac3f7374a
Whitney Walker <whimsicalwhitney@gmail.com>
2017-11-03 18:14:05 -0700

    Spin up new server to receive external requests

---
+++ externalServer/server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var helpers = require('./helpers.js')
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+
+app.get('/', function(req, res){
+  res.send('hello world');
+});
+
+app.post('/newgame', helpers.sendToQ, function(req, res){
+  console.log(req.body)
+  res.send('game received');
+});
+
+app.listen(2020);
