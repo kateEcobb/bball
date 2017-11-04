884fc67f80d8fa8891406f5ac1bba6a405831b98
Whitney Walker <whimsicalwhitney@gmail.com>
2017-11-03 18:13:21 -0700

    Implement route to send message to sqs

---
+++ externalServer/helpers.js
@@ -0,0 +1,33 @@
+// Load the AWS SDK for Node.js
+var AWS = require('aws-sdk');
+// Load credentials and set the region from the JSON file
+AWS.config.loadFromPath('./config/config.json');
+
+// Create an SQS service object
+var sqs = new AWS.SQS({apiVersion: '2012-11-05'});
+
+var sendToQ = (req, res, next) => {
+  // var body
+
+  // req.body
+  var params = {
+    DelaySeconds: 10,
+    MessageBody: JSON.stringify(req.body),
+    QueueUrl: "https://sqs.us-east-1.amazonaws.com/630349627985/playerRating"
+   };
+   
+   sqs.sendMessage(params, function(err, data) {
+     if (err) {
+       console.log("Error", err);
+       next();
+     } else {
+       console.log("Success", data.MessageId);
+       next();
+     }
+   });
+
+}
+
+
+// sendToQ()
+module.exports.sendToQ = sendToQ;
